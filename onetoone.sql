ef7e5a6e298dc76c9bfbfeaaa3fe5887ad48c42f
ulvi-huseynov <ulvhi.1769@gmail.com>
2024-12-07 23:16:29 +0400

    initialized sql queries

--- onetoone.sql
+++ onetoone.sql
@@ -4,7 +4,7 @@ CREATE TABLE customers (
         last_name VARCHAR(35));
 CREATE TABLE investors (
         investor_id BIGSERIAL PRIMARY KEY,
-        customer_id INT UNIQUE,
+        customer_id BIGSERIAL UNIQUE,
         profile_data VARCHAR(256),
         amount NUMERIC(16,2),
             FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
